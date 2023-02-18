28d863548cf14fde13d0c4ad892cd54297a0d13e
danyw <danielwiejak@gmai.com>
2023-02-18 12:54:39 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import NavBar from "./Components/NavBar";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./Pages/Home";
+import Random from "./Pages/Random";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App box-border w-screen md:px-5 ">
+        <header className="App-header flex flex-row justify-between">
+          <h1 className="">Pic Search</h1>
+          <NavBar />
+        </header>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/random" element={<Random />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Pages/Random.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Random = () => {
+
+    return (
+        <div className="random">
+        <h2>Random</h2>
+        </div>
+    );
+    };
+
+export default Random;
